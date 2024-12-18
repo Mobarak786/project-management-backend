bbee16e37ef1b7109dca28c60a7c5c63bfd411be
mobarak <mobarakansary42@gmail.com>
2024-12-18 21:30:14 +0530

    bug fixes

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import projectRoutes from './routes/projects';
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors());
