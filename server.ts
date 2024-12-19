1529e926ca806d4b9f7f5ff79cbfe4e8125ef650
mobarak <mobarakansary42@gmail.com>
2024-12-19 11:57:10 +0530

    cors policy changed

--- server.ts
+++ server.ts
@@ -3,16 +3,21 @@ import cors from 'cors';
 import projectRoutes from './routes/projects';
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://project-manage-frontend.netlify.app/'
+  origin: ['https://project-management-frontend-rho.vercel.app', 'http://localhost:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api', projectRoutes);
+
 
 // Health check route
 app.get('/', (req, res) => {
