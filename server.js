a57bc6954c4255f5b859db1205a7078ad9a1b824
mobarak <mobarakansary42@gmail.com>
2024-12-19 23:27:53 +0530

    cors bugs fixes

--- server.js
+++ server.js
@@ -8,10 +8,11 @@ const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
   origin: [
-    "http://localhost:5173",
     "https://project-manage-frontend.netlify.app/",
+    "http://localhost:5173",
   ],
-  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  headers: ["Content-Type", "Authorization"],
 };
 // Middleware to parse JSON requests
 app.use(express.json());
